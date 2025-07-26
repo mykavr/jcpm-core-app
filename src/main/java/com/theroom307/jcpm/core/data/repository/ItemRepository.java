4becef5f8c1443f1c0d9b80aefc83b9f57fdc52b
Mykhailo Avramenko <112896523+mykavr@users.noreply.github.com>
2025-07-26 17:20:56 +0300

    JCPM-63 Product Modified timestamp changes on each change of components (#34)

--- src/main/java/com/theroom307/jcpm/core/data/repository/ItemRepository.java
+++ src/main/java/com/theroom307/jcpm/core/data/repository/ItemRepository.java
@@ -2,6 +2,18 @@ package com.theroom307.jcpm.core.data.repository;
 
 import com.theroom307.jcpm.core.data.model.Item;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.time.ZonedDateTime;
 
 public interface ItemRepository<T extends Item> extends JpaRepository<T, Long> {
+
+    @Transactional
+    @Modifying
+    @Query("update #{#entityName} t set t.modified = :now where t.id = :id")
+    void updateModified(@Param("id") Long id, @Param("now") ZonedDateTime now);
+
 }
